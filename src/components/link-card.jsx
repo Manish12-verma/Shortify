295eca0ce4f863a5c27033a14dcc93662e1e5e0c
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-09-03 01:22:42 +0530

    added download qr functionality

--- src/components/link-card.jsx
+++ src/components/link-card.jsx
@@ -1,9 +1,27 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from "react-router-dom";
-import { Copy, Delete, Download } from "lucide-react";
+import { Copy,  Download, Trash } from "lucide-react";
 import { Button } from "./ui/button";
+
+
+
 const LinkCard = ({ url, fetchUrls }) => {
+
+    const downloadImage=()=>{
+        const imageUrl =  url?.qr;
+        const fileName = url?.title;
+
+            const anchor  = document.createElement("a");
+            anchor.href = imageUrl;
+            anchor.download = fileName
+
+            document.body.appendChild(anchor);
+                anchor.click()
+
+            document.body.removeChild(anchor);    
+    }
+
     return (
       <div className="flex flex-col md:flex-row gap-5 border  p-4 bg-gray-900 rounded-lg">
         <img src={url?.qr} className="h-32 object-contain ring ring-blue-500 self-start" alt="qr" />
@@ -27,12 +45,12 @@ const LinkCard = ({ url, fetchUrls }) => {
              <Copy/>
              
              </Button >
-             <Button variant="ghost">
+             <Button variant="ghost" onClick={downloadImage}>
              <Download/>
              
              </Button>
              <Button variant="ghost">
-             <Delete/>
+             <Trash/>
              
              </Button>
         </div>
